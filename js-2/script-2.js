07b4d70aa80f800ca5c735183e8891374306ef14
adele <adele.r87@gmail.com>
2021-01-07 16:06:57 +0100

    pari o dispari

--- js-2/script-2.js
+++ js-2/script-2.js
@@ -7,6 +7,8 @@
 
 // creo due finzioni . una che genera il numero random e l altra che mi dice se la somma dei numeri miei e del pc sono pari o dispari
 
+// è importante ricordare che se si dichiara una variabile all interno di una funzione poi non si puo riustilizzare quella variabile infatti darà come risultato un undefined --- vedi variabile scelta pc dichiarata dopo la funzione
+
 // chiedo all utente pari o Dispari e chiedo di scegliere un numero da 1 a 5
 
 var pariOdispari = prompt('pari o dispari')
@@ -18,11 +20,11 @@ var min = 1;
 var max = 5;
 
  function randomNum(min,max) {
-  var sceltaPc = Math.floor(Math.random() * (max + 1 - min) + min)
-  return sceltaPc;
+  return Math.floor(Math.random() * (max + 1 - min) + min)
  }
+ var sceltaPc = randomNum(min,max)
 
-console.log(randomNum(1, 5));
+console.log(sceltaPc);
 
  // creo una variabile che sara' la mia somma
  var somma = sceltaUtente + sceltaPc
@@ -38,8 +40,10 @@ console.log(randomNum(1, 5));
  }
 
  // dichiaro chi ha vinto : se la scelta è stata pari e la somma è pari vicnce l utente altrimenti il pc
- if (pariOdispari === pari && isPari(somma)) {
+ if (pariOdispari === 'pari' && isPari(somma)) {
    console.log('ha vinto');
  }else {
    console.log('hai perso');
  }
+
+ console.log(somma);
